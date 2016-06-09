01cef9f25437a0643dd42e124f25c376f18ce85f
YiZhi Liu <chn.lyzasd@gmail.com>
2016-06-09 11:01:33 +0800

    create

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by command.Zi on 16/6/7.
+//  Copyright © 2016年 command.Zi. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+    
+}
